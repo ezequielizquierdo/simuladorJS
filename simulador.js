39ae425006d5af7054efea2a003a815731208e8f
Ezequiel Izquierdo <ezequielizquierdo@hotmail.com>
2021-01-27 01:20:22 -0300

    "Subida con archivo de estilos"

--- simulador.js
+++ simulador.js
@@ -1,50 +1,56 @@
-///// COTIZADOR DE MATERIALES ########//
-// Consultar materiales hasta que no quiera seguir consultando.
+class Producto {
+    constructor(nombreProducto, descripcion, precioProducto, um) {
+        this.producto = nombreProducto;
+        this.descripcion = descripcion;
+        this.precio = precioProducto;
+        this.um = um;
+    }
+}
+
+let productoUno = new Producto("cemento", "Cemento portland normal bolsa de 50 kg", 608.22, "bl");
+let productoDos = new Producto("cal", "Cal común en bolsa x 25 kg", 261.24, "bl");
+let productoTres = new Producto("adhesivo" ,"Adhesivo tipo Klaukol bolsa 30 kg", 847.86, "bl");
+let productoCuatro = new Producto("cascote", "Cascote molido limpio x m3", 1696.02, "m3");
+let productoCinco = new Producto("revoque", "Revoque Plástico tipo 'Revear Color' tambor x 25 kg", 7780.59, "m2");
+let productoSeis = new Producto("arena", "Arena", 1845.16, "m3");
+
+const baseDeDatos = [productoUno, productoDos, productoTres, productoCuatro, productoCinco, productoSeis];
 
 var nombre = prompt("Ingrese su nombre");
 alert("Bienvenido " + nombre + ". Vamos a cotizar!");
 
-function cotizacion () {
+function cotizacion (){
 
-    var materiales = [];
-    materiales["cemento"] = ["Cemento portland normal bolsa de 50 kg", 608.22];
-    materiales["cal"] = ["Cal común en bolsa x 25 kg", 261.24];
-    materiales["adhesivo"] = ["Adhesivo tipo Klaukol bolsa 30 kg", 847.86];
-    materiales["cascote"] = ["Cascote molido limpio x m3", 1696.02];
-    materiales["revoque"] = ["Revoque Plástico tipo 'Revear Color", 7780.59];
-    materiales["arena"] = ["Arena", 1845.16];
-    materiales["acero"] = ["Acero de construcción tipo III diám. 12 mm", 77756.97];
-    materiales["ceramica"] = ["Cerám. Esmalt. 'Pietra Grafito' o similar 20 x 20 cm", 37971];
-
-    var continuar1 = 'si';
+    var material = prompt("Que material queres consultar?").toLowerCase();
     
-    do {
-        var material = prompt("Que material queres consultar?").toLowerCase();
+    const found = baseDeDatos.find(elemento => elemento.producto.toLowerCase() == material);
+    
+    console.log(found);
 
-        if (materiales[material] == undefined) {
-            alert("No tenemos ese material en stock");
-            continuar1 = prompt("Quieres consultar algun otro material? Si o No").toLowerCase();        
-        }
-
-    } while (materiales[material] == undefined && continuar1 == 'si');
-
-    if (continuar1 == 'si') {
-
-        console.log('Material consultado: ', materiales[material]);
+    if(!found){
+        alert("No tenemos ese producto");
+        console.log("El Material elegido no está en el inventario");
         
-        var coti = parseInt(prompt("Que cantidad queres cotizar?").toLowerCase());
-
-        console.log('Valor $: ', materiales[material][1] * coti);           
-
-        var continuar2 = prompt("Quieres consultar algun otro material? Si o No").toLowerCase();
-
-        if(continuar2 === "si"){
+        material = prompt("Quieres consultar algun otro material? Si o No").toLowerCase();
+        if(material === "si"){
             cotizacion();
         } else {
             alert("Muchas gracias por consultar");
-        }        
+        }
+
     } else {
-       alert("Muchas gracias por consultar");
+        
+        var coti = parseInt(prompt("Que cantidad queres cotizar?"));
+        console.log("Tu consulta por", found.descripcion, "por", coti, found.um, "es $: ", (coti * found.precio));  
+            
+        material = prompt("Quieres consultar algun otro material? Si o No").toLowerCase();
+           
+        if(material === "si"){
+            cotizacion();
+        } else {
+            alert("Muchas gracias por consultar");
+        }
     }
 }
-cotizacion();
+
+cotizacion(); 
