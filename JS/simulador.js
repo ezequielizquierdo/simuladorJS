03eb85eece7bbe906830afe60d0577625788f410
ezequielizquierdo <you@example.com>
2021-02-15 16:40:55 -0300

    "Agregado de JQuery al proyecto""

--- JS/simulador.js
+++ JS/simulador.js
@@ -1,23 +1,26 @@
 let baseDeDatos = [];
-let cotizador = [];
+let carrito = [];
+
+if (localStorage.getItem("carrito") != null) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    $("#contador").html(carrito.length);
+}
 
 class Producto {
-    constructor(nombreProducto, descripcion, precioProducto, um, imagen) {
+    constructor(nombreProducto, descripcion, precioProducto, um) {
         this.producto = nombreProducto;
         this.descripcion = descripcion;
         this.precio = precioProducto;
         this.um = um;
-        this.imagen = imagen;
     }
 }
 
-let productoUno = new Producto("cemento", "Cemento portland normal bolsa de 50 kg", 608.22, "bl", "https://www.rocasyminerales.net/wp-content/uploads/2018/08/Cemento.jpg");
-let productoDos = new Producto("cal", "Cal común en bolsa x 25 kg", 261.24, "bl", "https://lh3.googleusercontent.com/proxy/8A9NpNJ3yOyLTQWmCAxVpUo7w05cg7V_Y2fAPgNwuJq8R5umU9uxD_bk9YlcrynE1RMxQJc0lrc-n5URAfbXYe_2loLyUInGaoJCZsIT-2rW_ZNte2EbFQ");
-let productoTres = new Producto("adhesivo", "Adhesivo tipo Klaukol bolsa 30 kg", 847.86, "bl", "https://giliycia.com.ar/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/7/0/7066_01.jpg");
-let productoCuatro = new Producto("cascote", "Cascote molido limpio x m3", 1696.02, "m3", "http://http2.mlstatic.com/D_146021-MLA20695436900_052016-O.jpg");
-let productoCinco = new Producto("revoque", "Revoque Plástico tipo 'Revear Color' tambor x 25 kg", 7780.59, "m2", "https://http2.mlstatic.com/D_NQ_NP_735336-MLA44718019961_012021-V.jpg");
-let productoSeis = new Producto("arena", "Arena", 1845.16, "m3", "https://http2.mlstatic.com/D_NQ_NP_882262-MLA41783229132_052020-V.jpg");
-
+let productoUno = new Producto("cemento", "Cemento portland normal bolsa de 50 kg", 608.22, "bl");
+let productoDos = new Producto("cal", "Cal común en bolsa x 25 kg", 261.24, "bl");
+let productoTres = new Producto("adhesivo", "Adhesivo tipo Klaukol bolsa 30 kg", 847.86, "bl");
+let productoCuatro = new Producto("cascote", "Cascote molido limpio x m3", 1696.02, "m3");
+let productoCinco = new Producto("revoque", "Revoque Plástico tipo 'Revear Color' tambor x 25 kg", 7780.59, "m2");
+let productoSeis = new Producto("arena", "Arena", 1845.16, "m3");
 
 baseDeDatos.push(productoUno);
 baseDeDatos.push(productoDos);
@@ -27,84 +30,134 @@ baseDeDatos.push(productoCinco);
 baseDeDatos.push(productoSeis);
 
 
-function saludo() {
-    let nombre = prompt("Ingrese su nombre");
-    if (nombre) {
-        console.log(nombre);
+if (localStorage.nombre) {
+    $("#nombre-usuario").html('Hola ' + localStorage.nombre + '. Vamos a cotizar!');
+} else {
+    function saludo() {
+        let nombre = prompt("Ingrese su nombre");
         localStorage.setItem("nombre", nombre);
-        document.getElementById("nombre-usuario").innerHTML = 'Hola ' + nombre + '. Vamos a cotizar!';
-    } else {
-        document.getElementById("nombre-usuario").innerHTML = 'Hola amigo. No se tu nombre, pero vamos a cotizar!';
-    }
+        $("#nombre-usuario").html('Hola ' + localStorage.nombre + '. Vamos a cotizar!');
+    }     
+    saludo();
 }
-saludo();
 
 
+function Carrito() { // CARRITO VACIO
+    this.items = []
+
+    this.agregarItem = function (item) {
+        this.items.push(item);
+    }
+
+    this.valorTotal = function () {
+        var sumaTotal = 0;
+        for (let i = 0; i < this.items.length; i++) {
+            var valorlineas = unalinea.valorTotal();
+            sumaTotal += valorlineas
+        }
+        console.log(sumaTotal)
+        return sumaTotal;
+        
+        
+    }
+
+};
+
+var miCarrito = new Carrito();
+
+// Función para mostrar descripción del producto seleccionado.
 function mostrarDescripcion(event) {
     let descri = event.target.value;
     let found = baseDeDatos.find(elemento => elemento.producto == descri);
-
-    document.getElementById("demo").innerHTML = found.descripcion;
-
+    // document.getElementById("demo").innerHTML = found.descripcion;
+    $("#demo").html(found.descripcion);
     console.log(descri);
 }
 
+// Función para mostrar los valores (en $, USD, y EUR) de acuerdo a la cantidad ingresada
 function mostrarValor(event) {
-    let montpes = event.target.value;
+    let cantidadElegida = event.target.value;
     let prueba = demo.innerText;
     let found = baseDeDatos.find(elemento => elemento.descripcion == prueba);
 
-    document.getElementById("valorPesos").innerHTML = (montpes * found.precio).toFixed(2);
-    document.getElementById("valorDolares").innerHTML = ((montpes * found.precio) / document.getElementById("cotidolar").innerHTML).toFixed(2);
-    document.getElementById("valorEuros").innerHTML = ((montpes * found.precio) / document.getElementById("cotieuro").innerHTML).toFixed(2);
+    // var valorElegido = document.getElementById("valorPesos").innerHTML = (cantidadElegida * found.precio).toFixed(2);
+    let valorElegidoEnPes = $("#valorPesos").html((cantidadElegida * found.precio).toFixed(2));
+    // document.getElementById("valorDolares").innerHTML = ((cantidadElegida * found.precio) / document.getElementById("cotidolar").innerHTML).toFixed(2);
+    let valorElegidoEnUSD = $("#valorDolares").html(((cantidadElegida * found.precio) / $("#cotidolar").html()).toFixed(2));
+    // document.getElementById("valorEuros").innerHTML = ((cantidadElegida * found.precio) / document.getElementById("cotieuro").innerHTML).toFixed(2);
+    let valorElegidoEnEur = $("#valorEuros").html(((cantidadElegida * found.precio) / $("#cotieuro").html()).toFixed(2));
 
-    // console.log(prueba);
-    // console.log(found);
-    // console.log(montpes * found.precio);
-    // console.log(found.precio);
+
+    console.log(cantidadElegida)
+    // console.log(valorElegidoEnPes)
+    // console.log(valorElegidoEnUSD)
+    // console.log(valorElegidoEnEur)
+}
+
+//////////////////////////// PRUEBAS 
+
+////////// COTIZADOR
+// Que tenga las lineas de los productos que el usuario quiere agregar en su cotización
+// Tendrá que verse Descripción, cantidad, precio.
+// Poder modificar las cantidades desde el cotizador.
+// Poder borrar lineas desde el cotizador.
+// Que cada linea la cotice en pesos, dolares y euros.
+
+
+////////// CONSTRUCCION DE LINEAS 
+
+function agregarAlCotizador() {
+
+    function Linea() {
+        this.descripcion = []
+        this.cantidad = []
+        this.valor = []
+        this.total = []
+
+        this.agregarDescripcion = function (descripcion) {
+            this.descripcion = descripcion
+        }
+
+        this.agregarCantidad = function (cantidad) {
+            this.cantidad = cantidad
+        }
+
+        this.agregarValor = function (valor) {
+            this.valor = valor
+        }
+
+        this.valorTotal = function(){
+            var sumaTotal = 0; // Inicializo en 0
+            sumaTotal += this.producto.precioProducto; // Le sumo el valor que sale el producto.
+        }
+    };
+
+    ////////// ARMADO DE LINEA
+
+    var miLinea = new Linea();
+
+    // miLinea.agregarDescripcion(document.getElementById("demo").innerHTML);
+    miLinea.agregarDescripcion($("#demo").html());
+    // miLinea.agregarCantidad(document.getElementById("volumen").innerHTML);
+    miLinea.agregarCantidad($("#volumen").html());
+    // miLinea.agregarValor(document.getElementById("valorPesos").innerHTML);
+    miLinea.agregarValor($("#valorPesos").html());
+
+    console.log(miLinea);
+
+    miCarrito.agregarItem(miLinea);
+    console.log(miCarrito);
+
+    ////////// AGREGAR LINEAS AL COTIZADOR EN EL MODAL
+    
+    carrito.push(miLinea)
+    console.log(carrito)
 }
 
 
+////////// BORRAR CARRITO
 
 
 
 
-
-
-
-////////////////////////////
-////////////////////////////
-////////////////////////////
-
-///////////////// AGREGAR AL CARRITO
-
-// if (localStorage.getItem("cotizador") != null) {
-//     cotizador = JSON.parse(localStorage.getItem("cotizador")); // Valido si el carrito tiene valores.
-//     document.getElementById("cotizador").innerHTML = cotizador.length; // Esto debe estar cuando se inicializa el carrito, cuando se agrega un producto, y cuando se borra un producto.
-// }
-////////////////
-
-
-///////////////// FUNCIONES PARA AGREGAR PRODUCTOS
-
-// function agregarAlCotizador(producto) {
-//     cotizador.push(producto); // Agregará el producto a cotización.
-//     // console.log(carrito); // Imprimirá el carrito con los productos que vaya agregando.
-//     localStorage.setItem("cotizador", JSON.stringify(cotizador)); // Creamos un localStorage para guardar el carrito por mas que se cierre la ventana.
-//     // precio-total
-
-//     let aux = [];
-//     for (let i = 0; i < cotizador.length; i++) {
-//         // aux += cotizador[i].precioProducto;
-//     console.log(aux);
-//     }
-
-//     // document.getElementById("precio-total").innerHTML = "U$D" + aux;
-//     document.getElementById("contador").innerHTML = cotizador.length // Esto debe estar cuando se inicializa el carrito, cuando se agrega un producto, y cuando se borra un producto.
-
-// }
-
-// function agregarAlCotizador(){
-//     let valoresDelCotizador = localStorage.setItem("cotizador", JSON.stringify(cotizador));
-
-// }
+////////// BORRAR LINEAS DEL COTIZADOR EN EL MODAL
